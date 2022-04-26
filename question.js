359ec9d24bdf650e577a07a0b864047b67ef66c9
Abdulfatai Quadri Adewale <75619825+abujafar93@users.noreply.github.com>
2022-04-26 13:53:43 +0100

    Add files via upload

---
+++ question.js
@@ -0,0 +1,10 @@
+var container = document.getElementById("cont");
+
+
+function openSignIn(){
+  container.style.transform = "rotateY(-180deg)";
+}
+
+function openSignUp(){
+  container.style.transform = "rotateY(0deg)";
+}
